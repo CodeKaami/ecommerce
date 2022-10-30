ae2ee5aaebfa043f6ed092d9a2934abdae8652e1
CodeKaami <kamran1755@gmail.com>
2022-10-30 21:51:07 +0530

    created all  api

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getAllProducts,createProduct } = require("../controllers/productController");
+const { getAllProducts,createProduct,updateProduct,deleteProduct,getProductdetails } = require("../controllers/productController");
 
 const router = express.Router();
 
@@ -7,6 +7,7 @@ const router = express.Router();
 router.route("/products").get(getAllProducts);
 
 router.route("/products/new").post(createProduct);
+router.route("/product/:id").put(updateProduct).delete(deleteProduct).get(getProductdetails)
 
 module.exports = router
 
